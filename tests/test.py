c0ca1b3c57405af17802236f971bcd1177c57dac
Raul Molina <raul.molina@usalesiana.edu.bo>
2025-10-01 16:00:08 -0400

    primer endpoint

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    res = client.get("/")
+    assert res.status_code == 200
+    assert res.get_json() == {"message": "Hola, mundo desde Flask con GitHub Actions!"}
